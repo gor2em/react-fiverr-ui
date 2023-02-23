a340e217ce103e66c2fecb707fdb82ce3748f229
mekrograca <grkem.squash99@gmail.com>
2023-02-23 13:58:58 +0300

    home features

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -18,6 +18,89 @@ const Home = () => {
           <CatCard key={card.id} item={card} />
         ))}
       </Slide>
+
+      <div className="features">
+        <div className="container">
+          <div className="item">
+            <h1>A whole world of freelance talent at your fingertips</h1>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+              ipsam consequuntur cumque tempora.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+              ipsam consequuntur cumque tempora.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+              ipsam consequuntur cumque tempora.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+              ipsam consequuntur cumque tempora.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+              ipsam consequuntur cumque tempora.
+            </p>
+          </div>
+          <div className="item">
+            <video src="./img/video.mp4" controls></video>
+          </div>
+        </div>
+      </div>
+
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+            <h1>fiverr business</h1>
+            <h1>A business solution designed for teams</h1>
+            <p>
+              Upgrade to a curated exerience packed with tools and benefits,
+              dedicated to businesses
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Connect to freelancers with proven business experience
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Connect to freelancers with proven business experience
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Connect to freelancers with proven business experience
+            </div>
+            <button>Explore Fiver Business</button>
+          </div>
+          <div className="item">
+            <img
+              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_1.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624757/business-desktop-870-x1.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
